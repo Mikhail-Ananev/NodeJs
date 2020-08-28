c0a25527e836c681c920650aef50487259a44594
mic-warrior <mic-warrior@yandex.ru>
2020-08-29 01:05:55 +0400

    Cosmetic changes

--- loaders/index.js
+++ loaders/index.js
@@ -2,13 +2,13 @@ import routerLoader from "./routerLoader";
 import { sequelize } from "../config/db";
 
 export function loaders(app) {
-  sequelize
-  .authenticate()
+  sequelize.authenticate()
   .then(() => {
       console.log('Connection to database!');
   })
   .catch(err => {
       console.log('Can\'t connect to database!');
   });
+
   routerLoader(app);
 };
